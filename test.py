c3d589f6744c335d5744f29b53cd03f075326826
hyuan01 <79280968+hyuan01@users.noreply.github.com>
2022-04-09 09:38:46 -0400

    Add files via upload
    testing

---
+++ test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from time import sleep
+chrome_options = ChromeOptions()
+chrome_options.add_extension('test_src.crx')
+
+
+driver = webdriver.Chrome('D:/chromedriver/chromedriver', options = chrome_options)
+driver.get('https://google.com')
+sleep(3)
+driver.get('https://youtube.com')
+sleep(3)
+driver.get('chrome-extension://hhgbhcehbginhikalilbcpbpfedhnaag/analysis.html')
+sleep(3)
+driver.get('chrome-extension://hhgbhcehbginhikalilbcpbpfedhnaag/popup.html')
+input('Press [Enter] To close browser')
+driver.quit()
